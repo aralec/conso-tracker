838a8399a974303876d7d78823c7cc8c32ae10cd
Arnaud <arnaudralec@gmail.com>
2024-10-06 11:50:52 +1100

    🚧 feat: processing csv

--- src/internal/core/services/csv-service.go
+++ src/internal/core/services/csv-service.go
@@ -17,27 +17,55 @@ func (cs *CsvService) ExtractData(file multipart.File) {
 	reader := csv.NewReader(file)
 	reader.Comma = ';'
 
-	// En-têtes du fichier :
-	headers, err := reader.Read()
-	if err != nil {
-		fmt.Println("Error while reading file :", err)
+	// Ignorer les lignes vides (en-têtes et résumé) jusqu'à ce que nous trouvions une ligne avec des champs séparés par ';'
+	// Si la ligne contient un seul champs, c'est un commentaire
+	headers, abort := cs.readHeaders(reader)
+	if abort {
 		return
 	}
 
-	// Afficher les en-têtes
-	fmt.Println("\n\nHeaders are :\n", headers)
-
 	// Lire les données du fichier
+	data, abort := cs.readBody(reader, headers)
+	if abort {
+		return
+	}
+
+	// Afficher les données
+	for _, entry := range data {
+		fmt.Println(entry)
+	}
+}
+
+func (*CsvService) readHeaders(reader *csv.Reader) ([]string, bool) {
+	var headers []string
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("Header error :", err)
+			return nil, true
+		}
+
+		if len(record) != 1 {
+			headers = record
+			break
+		}
+	}
+	return headers, false
+}
+
+func (*CsvService) readBody(reader *csv.Reader, headers []string) ([]map[string]string, bool) {
 	var data []map[string]string
 	for {
 		record, err := reader.Read()
 		if err == io.EOF {
-			// Fin de ligne
 			break
 		}
 		if err != nil {
-			fmt.Println("Error while reading line : ", err)
-			return
+			fmt.Println("Body error :", err)
+			return nil, true
 		}
 
 		entry := make(map[string]string)
@@ -47,9 +75,5 @@ func (cs *CsvService) ExtractData(file multipart.File) {
 		}
 		data = append(data, entry)
 	}
-
-	// Afficher les données
-	for _, entry := range data {
-		fmt.Println(entry)
-	}
+	return data, false
 }
